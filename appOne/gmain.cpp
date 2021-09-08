e9d4ed764e1fe97347f36dcc47a30dd03629c885
Coding-Ocean <eijo524@gmail.com>
2021-09-08 17:29:45 +0900

    update step

--- appOne/gmain.cpp
+++ appOne/gmain.cpp
@@ -1,159 +1,265 @@
+//#define _IK_EXERCISE
 //#define _IK_BASIC
 //#define _IK_BASIC_FIXED_ROOT
-#define _IK_TENTACLE
+//#define _IK_TENTACLE
+#define _IK_CLASS_TENTACLE
 
+#ifdef _IK_EXERCISE
+#include"libOne.h"
+class SEG {
+public:
+    VECTOR2 sp, ep;
+    float len = 0;
+};
+void gmain() {
+    window(1000, 1000);
+    while (notQuit) {
+        clear();
+    }
+}
+#endif
 #ifdef _IK_BASIC
 #include"libOne.h"
 class SEG {
 public:
-    VECTOR2 Sp, Ep;
-    float Len=0;
+    VECTOR2 sp, ep;
+    float len=0;
     void follow(const VECTOR2& pos) {
-        VECTOR2 dir = pos - Sp;
-        dir.setMag(Len);
+        ep = pos;
+        //始点を求める
+        VECTOR2 dir = pos - sp;
+        dir.setMag(len);
         dir *= -1;
-        Sp = pos + dir;
-        Ep = pos;
+        sp = pos + dir;
     }
-    void drawPoint(){
-        point(Sp.x, Sp.y);
-    }
-    void drawLine() {
-        line(Sp.x, Sp.y, Ep.x, Ep.y);
+    void draw() {
+        strokeWeight(6);
+        line(sp.x, sp.y, ep.x, ep.y);
+        strokeWeight(18);
+        point(sp.x, sp.y);
     }
 };
 
 void gmain() {
     window(1000, 1000);
-    const int numSegs = 5;
+
+    VECTOR2 center(width / 2, height / 2);
+    
+    const int numSegs = 6;
     SEG segs[numSegs];
     for (int i = 0; i < numSegs; i++) {
-        segs[i].Len = 50;
+        segs[i].sp = center;
+        segs[i].ep = center;
+        segs[i].len = 50;
     }
+    
+    VECTOR2 mouse = center;
+    
     while (notQuit) {
         clear();
-        
-        VECTOR2 mouse(mouseX, mouseY);
-        
-        int i = numSegs - 1;//最後のseg
-        segs[i].follow(mouse);//最後のsegはマウスを追う
-        while (i > 0) {
-            i--;
-            segs[i].follow(segs[i + 1].Sp);//次のセグメントの始点を追う
+
+        if (isPress(MOUSE_LBUTTON)) {
+            mouse = VECTOR2(mouseX, mouseY);
         }
 
-        stroke(255,100,100);
+        //先端から根元に向かって、終点、始点を決めていく
+        int i = numSegs - 1;//先端ののsegのインデックス
+        segs[i].follow(mouse);//先端のsegはマウスを追う
+        while (i > 0) {
+            i--;
+            segs[i].follow(segs[i + 1].sp);
+        }
+
+        //根元から先端に向かって描画
+        stroke(255);
         for (int i = 0; i < numSegs; i++) {
-            strokeWeight(4);
-            segs[i].drawLine();
-            strokeWeight(10);
-            segs[i].drawPoint();
+            segs[i].draw();
         }
     }
 }
 #endif
-
 #ifdef _IK_BASIC_FIXED_ROOT
+//根っこを固定するバージョン
 #include"libOne.h"
 class SEG {
 public:
-    VECTOR2 Sp, Ep;
-    float Len = 0;
-    float Angle = 1.57f;
+    VECTOR2 sp, ep;
+    float len = 0;
+    float angle = 0;//これを追加
     void follow(const VECTOR2& pos) {
-        VECTOR2 dir = pos - Sp;
-        Angle = atan2(dir.y, dir.x);
-        dir.setMag(Len);
-        dir *= -1;
-        Sp = pos + dir;
-        Ep = pos;
+        ep = pos;
+        //始点を求める
+        VECTOR2 vec = pos - sp;
+        angle = atan2(vec.y, vec.x);//始点から終点へのangleを求めておく
+        vec.setMag(len);
+        vec *= -1;
+        sp = pos + vec;
     }
     void setPos(const VECTOR2 pos) {
-        Sp = pos;
-        //End position
-        VECTOR2 dir(cos(Angle), sin(Angle));
-        Ep = Sp + dir * Len;
+        sp = pos;
+        //angleから終点を求める
+        VECTOR2 dir(cos(angle), sin(angle));
+        ep = sp + dir * len;
     }
-    void drawPoint() {
-        point(Sp.x, Sp.y);
-    }
-    void drawLine() {
-        line(Sp.x, Sp.y, Ep.x, Ep.y);
+    void draw() {
+        strokeWeight(6);
+        line(sp.x, sp.y, ep.x, ep.y);
+        strokeWeight(18);
+        point(sp.x, sp.y);
     }
 };
 
 void gmain() {
     window(1000, 1000);
-
-    const int numSegs = 5;
+    VECTOR2 center(width / 2, height / 2);
+    
+    const int numSegs = 6;
     SEG segs[numSegs];
     for (int i = 0; i < numSegs; i++) {
-        segs[i].Len = 50;
+        segs[i].sp = center;
+        segs[i].ep = center;
+        segs[i].len = 50;
     }
+    
+    VECTOR2 mouse(center.x,center.y);
 
-    VECTOR2 rootPos(width / 2, height / 2);
-
+    VECTOR2 rootPos = center;
+    
     while (notQuit) {
         clear();
 
-        VECTOR2 mouse(mouseX, mouseY);
-
-        int i = numSegs - 1;//最後のseg
-        segs[i].follow(mouse);//最後のsegはマウスを追う
-        while (i > 0) {
-            i--;
-            segs[i].follow(segs[i + 1].Sp);//次のセグメントの始点を追う
+        if (isPress(MOUSE_LBUTTON)) {
+            mouse = VECTOR2(mouseX, mouseY);
         }
 
-        stroke(255, 100, 100);
+        int i = numSegs - 1;
+        segs[i].follow(mouse);
+        while (i > 0) {
+            i--;
+            segs[i].follow(segs[i + 1].sp);
+        }
+
+        stroke(255);
         for (int i = 0; i < numSegs; i++) {
             if (i == 0)
                 segs[i].setPos(rootPos);
             else
-                segs[i].setPos(segs[i - 1].Ep);
-            strokeWeight(4);
-            segs[i].drawLine();
-            strokeWeight(10);
-            segs[i].drawPoint();
+                segs[i].setPos(segs[i - 1].ep);
+            segs[i].draw();
         }
     }
 }
 #endif
-
 #ifdef _IK_TENTACLE
+//触手に見えるようにする
+#include"libOne.h"
+class SEG {
+public:
+    VECTOR2 sp, ep;
+    float len = 0;
+    float angle = 0;
+    void follow(const VECTOR2& pos) {
+        ep = pos;
+        //始点を求める
+        VECTOR2 vec = pos - sp;
+        angle = atan2(vec.y, vec.x);//始点から終点へのangleを求めておく
+        vec.setMag(len);
+        vec *= -1;
+        sp = pos + vec;
+    }
+    void setPos(const VECTOR2 pos) {
+        sp = pos;
+        //angleから終点を求める
+        VECTOR2 vec(cos(angle), sin(angle));
+        ep = sp + vec * len;
+    }
+    void draw() {
+        point(sp.x, sp.y);
+    }
+};
+
+void gmain() {
+    window(1000, 1000);
+    
+    VECTOR2 center(width / 2, height / 2);
+    float len = 2;
+    const int numSegs = 150;
+    SEG segs[numSegs];
+    for (int i = 0; i < numSegs; i++) {
+        segs[i].sp = center;
+        segs[i].ep = center;
+        segs[i].len = len;
+    }
+
+    VECTOR2 mouse(center.x, center.y);
+
+    VECTOR2 rootPos = center;
+
+    while (notQuit) {
+        clear();
+        //追いかける範囲をrootoPosを中心、半径を触手の長さの円の内側に制限
+        mouse = VECTOR2(mouseX, mouseY);
+        VECTOR2 fp = mouse - rootPos;
+        fp.limmit(len * numSegs);
+        fp += rootPos;//follow pos
+        //末端から根っこに向かって処理
+        int i = numSegs - 1;
+        segs[i].follow(fp);
+        while (i > 0) {
+            i--;
+            segs[i].follow(segs[i + 1].sp);
+        }
+        //根っこから末端に向かって、位置を再計算して描画
+        stroke(255);
+        for (int i = 0; i < numSegs; i++) {
+            if (i == 0)
+                segs[i].setPos(rootPos);
+            else
+                segs[i].setPos(segs[i - 1].ep);
+            float thickness = 50;
+            strokeWeight((numSegs - i) * thickness / numSegs);
+            segs[i].draw();
+        }
+    }
+}
+#endif
+#ifdef _IK_CLASS_TENTACLE
 #include"libOne.h"
 class SEG {
     friend class TENTACLE;
     VECTOR2 Sp, Ep;
     float Len = 0;
-    float Angle = 1.57f;
+    float Angle = 0;
 public:
     void follow(const VECTOR2& pos) {
+        Ep = pos;
+        //始点を決める
         VECTOR2 dir = pos - Sp;
         Angle = atan2(dir.y, dir.x);
         dir.setMag(Len);
         dir *= -1;
         Sp = pos + dir;
-        Ep = pos;
     }
     void setPos(const VECTOR2 pos) {
         Sp = pos;
-        //End position
+        //Angleを元に終点を求める
         VECTOR2 dir(cos(Angle), sin(Angle));
         Ep = Sp + dir * Len;
     }
-    void drawPoint() {
-        point(Sp.x, Sp.y);
-    }
-    void drawLine() {
-        line(Sp.x, Sp.y, Ep.x, Ep.y);
+    void draw(int lineFlag=false) {
+        if(lineFlag)
+            line(Sp.x, Sp.y, Ep.x, Ep.y);
+        else
+            point(Sp.x, Sp.y);
     }
 };
 class TENTACLE {
     int NumSegs = 0;
     SEG* Segs = 0;
     VECTOR2 RootPos;
-    float Len;
+    float TentacleLen=0;
+    float Thickness = 0;
+    COLOR Color;
 public:
     ~TENTACLE() {
         delete[] Segs;
@@ -164,46 +270,51 @@ public:
         for (int i = 0; i < NumSegs; i++) {
             Segs[i].Len = len;
         }
-        Len = len;
+        TentacleLen = len * numSegs;
         RootPos.x = rootX;
         RootPos.y = rootY;
+        Thickness = 50;
+        Color = COLOR(0, 200, 0);//RGB
     }
     void follow(const VECTOR2& pos) {
-        VECTOR2 vec = pos - RootPos;
-        vec.limmit(Len*NumSegs);
-
-        int i = NumSegs - 1;//最後のseg
-        Segs[i].follow(RootPos+vec);//最後のsegはマウスを追う
+        //末端が追いかける範囲を「rootoPosを中心、半径を触手とする円」の内側に制限
+        VECTOR2 fp = pos - RootPos;
+        fp.limmit(TentacleLen);
+        fp += RootPos;
+        //末端から根っこに向かって処理
+        int i = NumSegs - 1;//末端のsegのインデックス
+        Segs[i].follow(fp);
         while (i > 0) {
             i--;
-            Segs[i].follow(Segs[i + 1].Sp);//次のセグメントの始点を追う
+            Segs[i].follow(Segs[i + 1].Sp);
         }
     }
     void draw() {
-        stroke(6.28f/3,255,255);
+        colorMode(RGB);
+        stroke(Color);
+        //根っこから末端に向かって、位置を再計算して描画
         for (int i = 0; i < NumSegs; i++) {
             if (i == 0)
                 Segs[i].setPos(RootPos);
             else
                 Segs[i].setPos(Segs[i - 1].Ep);
-            strokeWeight((NumSegs-i+2)*0.8f);
-            Segs[i].drawLine();
-            //strokeWeight(10);
-            //Segs[i].drawPoint();
+            //描画
+            strokeWeight((NumSegs - i) * Thickness / NumSegs);
+            Segs[i].draw();
         }
     }
 };
 class BALL {
-    float Diameter;
+    float Radius;
     float Hue;
     VECTOR2 Pos, Vel;
     float Gravity;
 public:
     BALL() {
         Hue = 0;
-        Diameter = 50;
+        Radius = 20;
         Pos.x = 200;
-        Pos.y = 100;
+        Pos.y = 50;
         Vel.x = 7;
         Vel.y = -2.1f;
         Gravity = 0.08f;
@@ -211,142 +322,40 @@ public:
     VECTOR2 pos() {
         return Pos;
     }
-    //constructor() {
-    //    this.pos = createVector(390, 100);
-    //    this.dir = createVector(6, -2.1);
-    //    this.initDy = -8.6;
-    //    this.gravity = 0.1;
-    //}
     void move() {
         Vel.y += Gravity;
         Pos += Vel;
-        if (Pos.y > height - Diameter/2) {
-            Pos.y = height - Diameter/2;
+        if (Pos.y > height - Radius) {
+            Pos.y = height - Radius;
             Vel.y *= -1;
         }
         if (Pos.x<110 || Pos.x>width - 110) {
             Vel.x *= -1;
         }
     }
-    //move() {
-    //    this.pos.add(this.dir);
-    //    this.dir.y += this.gravity;
-    //    if (this.pos.y > height - 15) {
-    //        this.dir.y = this.initDy;
-    //    }
-    //    if (this.pos.x<110 || this.pos.x>width - 110) {
-    //        this.dir.x *= -1;
-    //    }
-    //}
     void draw() {
-        strokeWeight(Diameter);
+        strokeWeight(Radius * 2);
         colorMode(HSV);
-        stroke(Hue,64,255,200);
+        stroke(Hue, 64, 255, 220);
         Hue += 0.2f;
         point(Pos.x, Pos.y);
-        //noStroke();
-        //fill(220, 100, 100);
-        //circle(Pos.x, Pos.y, 30);
     }
-    //show() {
-    //    noStroke();
-    //    fill(220, 100, 100);
-    //    circle(this.pos.x, this.pos.y, 30);
-    //}
 };
 void gmain() {
-    window(900, 600);
-    TENTACLE tentacle[2];
-    tentacle[0].create(60, 5, 60, height / 2);
-    tentacle[1].create(60, 5, width-60, height / 2);
+    window(900, 600, full);
+    hideCursor();
+    TENTACLE tentacles[2];
+    tentacles[0].create(300, 1,       60, height / 2);
+    tentacles[1].create(300, 1, width-60, height / 2);
     BALL ball;
     while (notQuit) {
         clear();
         ball.move();
         for (int i = 0; i < 2; i++) {
-            tentacle[i].follow(ball.pos());
-            tentacle[i].draw();
+            tentacles[i].follow(ball.pos());
+            tentacles[i].draw();
         }
         ball.draw();
-    }
-}
-#endif
-
-#ifdef _IK_TENTA
-#include"libOne.h"
-class SEG {
-public:
-    VECTOR2 Sp, Ep;
-    float Len=0;
-    float Angle=1.57f;
-    void follow(VECTOR2 pos) {
-        VECTOR2 dir = pos - Sp;
-        Angle = dir.heading();
-        dir.setMag(Len);
-        dir *= -1;
-        Sp = pos + dir;
-    }
-    void setPos(VECTOR2 pos) {
-        Sp = pos;
-        //End position
-        VECTOR2 dir(cos(Angle), sin(Angle));
-        Ep = Sp + dir * Len;
-    }
-    void drawPoint() {
-        point(Sp.x, Sp.y);
-    }
-    void drawLine() {
-        line(Sp.x, Sp.y, Ep.x, Ep.y);
-    }
-};
-
-void gmain() {
-    window(1000, 1000);
-    VECTOR2 rootPos(0, height/2);
-    const int numSegs = 100;
-    SEG segs[numSegs];
-    for (int i = 0; i < numSegs; i++) {
-        segs[i].create(4, 0);
-    }
-    VECTOR2 ball,vec(5,10);
-    while (notQuit) {
-        clear();
-        
-        ball += vec;
-        strokeWeight(10);
-        stroke(255);
-        point(ball.x, ball.y);
-        if (ball.x < 0 || ball.x>width)vec.x *= -1;
-        if (ball.y < 0||ball.y>height)vec.y *= -1;
-
-        VECTOR2 mouse(mouseX, mouseY);
-        VECTOR2 range = ball - rootPos;
-        range.limmit(400);
-        VECTOR2 followPos = rootPos + range;
-
-        int i = numSegs - 1;
-        segs[i].follow(followPos);//最後のsegsはマウスを追う
-        while (i > 0) {
-            i--;
-            segs[i].follow(segs[i + 1].Sp);//次のセグメントの始点を追う
-
-            //segs[i].drawPoint();
-        }
-
-        stroke(200, 100, 150);
-        for (i = 0; i < numSegs; i++) {
-            if (i == 0)
-                segs[i].setPos(rootPos);
-            else
-                segs[i].setPos(segs[i - 1].Ep);
-
-
-            strokeWeight((numSegs - i));
-            if (i < numSegs - 20)
-                segs[i].drawPoint();
-            else
-                segs[i].drawLine();
-        }
     }
 }
 #endif
